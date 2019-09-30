dbe9d796b76cf228f223ea79dc681293b53d821b
crisner <christie.reni@gmail.com>
2019-09-30 21:39:00 +0530

    Add note

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -7,6 +7,7 @@ const ExpenseListItem = ({id, description, note, amount, createdAt}) => (
     <div>
         <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
         <p>{numeral(amount/100).format('0,0.00')}</p>
+        <p><small>{note ? note : null}</small></p>
         <p><small>{moment(createdAt).format('ll')}</small></p>    
     </div>
 );
